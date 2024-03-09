baccd50579bc37e690d54e9a1185eaa806292b5c
unknown <Breke64@gmail.com>
2024-03-08 22:09:39 -0500

    still facing error with heroicons in projects/about components

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -32,7 +32,7 @@ export default function About() {
         <img
           className="object-cover object-center rounded"
           alt="resume-hero"
-          src="./assets/resume.jpg"
+          src="../assets/resume.jpg"
         />
       </div>
     </div>
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -17,7 +17,7 @@ export default function Contact() {
           marginHeight={0}
           marginWidth={0}
           style={{ filter: "opacity(0.7)" }}
-          src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+          src="https://www.google.com/maps/embed/v1/place?q=Minneapolis,+MN,+USA&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
         />
         <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
           <div className="lg:w-1/2 px-6">
@@ -25,8 +25,8 @@ export default function Contact() {
               ADDRESS
             </h2>
             <p className="mt-1">
-              97 Warren St. <br />
-              New York, NY 10007
+              Minneapolis. <br />
+              Minneapolis, MN 10007
             </p>
           </div>
           <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -50,9 +50,7 @@ export default function Contact() {
         <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
           Hire Me
         </h2>
-        <p className="leading-relaxed mb-5">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-          suscipit officia aspernatur veritatis. Asperiores, aliquid?
+        <p className="leading-relaxed mb-5">Leading
         </p>
         <div className="relative mb-4">
           <label htmlFor="name" className="leading-7 text-sm text-gray-400">
