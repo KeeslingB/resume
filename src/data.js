9bc1dc9798d8777efb4e89b4b0765174c6a06c2a
unknown <Breke64@gmail.com>
2024-03-18 08:36:46 -0400

    added more skills in data js file for skills component

--- src/data.js
+++ src/data.js
@@ -60,4 +60,4 @@ export const projects = [
 
 // Skills 
 
-export const skills = ["Javascript","Express","Nodejs","Mongodb","MySQL","C","Vite","HTML/CSS"]
+export const skills = ["Javascript","Express","Nodejs","Mongodb","MySQL","C","Vite","HTML/CSS","Agile","Heroku/Netlify"]
